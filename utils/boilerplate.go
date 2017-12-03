0de74c502bb01d0fcb76294f8ffb923678ef6048
Graham Campbell <gcampbell@demonware.net>
2017-12-03 15:22:45 -0800

    Add boilerplate template
    Handy for copy-pasting boilerplate for each new puzzle

---
+++ utils/boilerplate.go
@@ -0,0 +1,27 @@
+package puzzle01
+
+import "github.com/parafiend/advent2017/base"
+
+const id = "1"
+
+type Puzzle struct{}
+
+func (p Puzzle) Test() string {
+	return "doin a test"
+}
+
+func (p Puzzle) Phase1() string {
+	return "doin phase1"
+}
+
+func (p Puzzle) Phase2() string {
+	return "doin phase2"
+}
+
+func (p Puzzle) String() string {
+	return "puzz" + id
+}
+
+func init() {
+	base.Register(id, Puzzle{})
+}
