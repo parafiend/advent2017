10ccc98184034cc84f3a52bf94adbde00a354b76
Graham Campbell <gcampbell@demonware.net>
2017-12-03 15:22:01 -0800

    Puzzle 2 Phase 2
    Pretty straightforward brute-forcing. Possibly a better algorithm with
    sorting and changing search space?

--- puzzle02/puzzle02.go
+++ puzzle02/puzzle02.go
@@ -16,7 +16,13 @@ func (p Puzzle) Test() string {
 	test := `5 1 9 5
 		7 5 3
 		2 4 6 8`
-	return strconv.Itoa(checksum(test))
+	log.Println(strconv.Itoa(checksum(test)))
+
+	test2 := `5 9 2 8
+	9 4 7 3
+	3 8 6 5`
+	log.Println(checksum2(test2))
+	return "what"
 }
 
 func (p Puzzle) Phase1() string {
@@ -24,7 +30,7 @@ func (p Puzzle) Phase1() string {
 }
 
 func (p Puzzle) Phase2() string {
-	return "doin phase2"
+	return strconv.Itoa(checksum2(Spreadsheet))
 }
 
 func (p Puzzle) String() string {
@@ -37,7 +43,7 @@ func toCells(input string) [][]string {
 	for _, v := range rows {
 		row := strings.Fields(v)
 		cells = append(cells, row)
-		log.Println(cells)
+		//log.Println(cells)
 	}
 	return cells
 }
@@ -58,6 +64,34 @@ func checksum(input string) int {
 		result += diff
 	}
 	log.Println(result)
+	return result
+}
+
+func checksum2(input string) int {
+	sheet := toCells(input)
+	var result int
+	for _, cells := range sheet {
+		var big, little int
+		var found bool
+		for i, cell := range cells {
+			curr, _ := strconv.Atoi(cell)
+			for j := i + 1; j < len(cells); j++ {
+				candidate, _ := strconv.Atoi(cells[j])
+
+				if curr%candidate == 0 || candidate%curr == 0 {
+					big, little = max(curr, candidate), min(curr, candidate)
+					found = true
+					break
+				}
+			}
+			if found {
+				divide := big / little
+				log.Println(little, big, divide)
+				result += divide
+				break
+			}
+		}
+	}
 	return result
 }
 
