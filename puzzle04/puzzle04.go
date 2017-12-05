3a5678037b10dede4af3272ed2be73c65e7912c0
Graham Campbell <gcampbell@demonware.net>
2017-12-04 19:08:00 -0800

    Puzzle 4 Phase 1
    Pretty straightforward. Split on whitespace and sort strings, then
    check neighbours for dupes

--- puzzle04/puzzle04.go
+++ puzzle04/puzzle04.go
@@ -1,17 +1,33 @@
-package utils
+package puzzle04
 
-import "github.com/parafiend/advent2017/base"
+import (
+	"log"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/parafiend/advent2017/base"
+)
 
 const id = "4"
 
 type Puzzle struct{}
 
 func (p Puzzle) Test() string {
+	log.Println(isPhraseValid("aa bb cc dd ee"))
+	log.Println(isPhraseValid("aa bb cc dd aa"))
+	log.Println(isPhraseValid("aa bb cc dd aaa"))
 	return "doin a test"
 }
 
 func (p Puzzle) Phase1() string {
-	return "doin phase1"
+	var sumValid int
+	for _, v := range strings.Split(Phrases, "\n") {
+		if isPhraseValid(v) {
+			sumValid++
+		}
+	}
+	return strconv.Itoa(sumValid)
 }
 
 func (p Puzzle) Phase2() string {
@@ -20,6 +36,19 @@ func (p Puzzle) Phase2() string {
 
 func (p Puzzle) String() string {
 	return "puzz" + id
+}
+
+func isPhraseValid(phrase string) bool {
+	phrases := strings.Fields(phrase)
+	sort.Strings(phrases)
+	valid := true
+	for i, v := range phrases {
+		if i < len(phrases)-1 && phrases[i+1] == v {
+			valid = false
+			break
+		}
+	}
+	return valid
 }
 
 func init() {
