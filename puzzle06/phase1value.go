e8cc5ac331de30b91beb6f3e305ccb819ccf8efb
Graham Campbell <gcampbell@demonware.net>
2017-12-06 21:20:13 -0800

    Day 6 both phases
    Brutal "hash" = string of memory banks. Didn't both to learn Go
    hashing, which would be way better.

---
+++ puzzle06/phase1value.go
@@ -0,0 +1,3 @@
+package puzzle06
+
+const rawBanks string = `4	1	15	12	0	9	9	5	5	8	7	3	14	5	12	3`
--- puzzle06/puzzle06.go
+++ puzzle06/puzzle06.go
@@ -1,25 +1,94 @@
 package puzzle06
 
-import "github.com/parafiend/advent2017/base"
+import (
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/parafiend/advent2017/base"
+)
 
 const id = "6"
 
 type Puzzle struct{}
 
 func (p Puzzle) Test() string {
-	return "doin a test"
+	testInput := `0 2 7 0`
+	count, delta := countRedistributes(testInput)
+	log.Println(count, delta)
+	return "lalalal"
 }
 
 func (p Puzzle) Phase1() string {
-	return "doin phase1"
+	count, _ := countRedistributes(rawBanks)
+	return strconv.Itoa(count)
 }
 
 func (p Puzzle) Phase2() string {
-	return "doin phase2"
+	_, delta := countRedistributes(rawBanks)
+	return strconv.Itoa(delta)
 }
 
 func (p Puzzle) String() string {
 	return "puzz" + id
+}
+
+func countRedistributes(input string) (int, int) {
+	banks := parseInput(input)
+	count := 0
+	delta := 0
+	seen := make(map[string]int)
+	for {
+		banks = redistribute(banks)
+		log.Println(banks)
+		hash := getHash(banks)
+		count++
+		if last, ok := seen[hash]; ok {
+			delta = count - last
+			break
+		}
+		seen[hash] = count
+	}
+	return count, delta
+}
+
+func getHash(banks []int) string {
+	var hash string
+	for _, v := range banks {
+		hash += strconv.Itoa(v) + ","
+	}
+	return hash
+}
+
+func parseInput(input string) []int {
+	var banks []int
+	for _, v := range strings.Fields(input) {
+		if bank, err := strconv.Atoi(v); err == nil {
+			banks = append(banks, bank)
+		}
+	}
+	return banks
+}
+
+func redistribute(banks []int) []int {
+	src, count := findBiggest(banks)
+	banks[src] = 0
+	for i := 1; i <= count; i++ {
+		target := (src + i) % len(banks)
+		banks[target]++
+	}
+	return banks
+}
+
+func findBiggest(banks []int) (int, int) {
+	idx, count := 0, -1
+	for i, v := range banks {
+		if v > count {
+			idx = i
+			count = v
+		}
+	}
+	return idx, count
 }
 
 func init() {
