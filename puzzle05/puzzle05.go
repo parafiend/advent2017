be09003d8b03fff5ca5357956591075bfd21e1a4
Graham Campbell <gcampbell@demonware.net>
2017-12-06 20:19:32 -0800

    Adding day 5 boilerplate

---
+++ puzzle05/puzzle05.go
@@ -0,0 +1,27 @@
+package utils
+
+import "github.com/parafiend/advent2017/base"
+
+const id = "5"
+
+type Puzzle struct{}
+
+func (p Puzzle) Test() string {
+	return "doin a test"
+}
+
+func (p Puzzle) Phase1() string {
+	return "doin phase1"
+}
+
+func (p Puzzle) Phase2() string {
+	return "doin phase2"
+}
+
+func (p Puzzle) String() string {
+	return "puzz" + id
+}
+
+func init() {
+	base.Register(id, Puzzle{})
+}
