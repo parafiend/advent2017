3116b33f4d7168ea5999b8d8813e984e1277cee8
Graham Campbell <gcampbell@demonware.net>
2017-12-10 11:46:00 -0800

    Day 9 Test and Phase 1
    Raw function streaming over runes. Though about making classes and
    building up entire state of stream but that seemed like overkill.
    Fingers crossed part 2 doesn't need it.

--- puzzle09/puzzle09.go
+++ puzzle09/puzzle09.go
@@ -1,17 +1,38 @@
 package puzzle09
 
-import "github.com/parafiend/advent2017/base"
+import (
+	"log"
+	"strconv"
+
+	"github.com/parafiend/advent2017/base"
+)
 
 const id = "9"
+
+type StreamParser struct {
+	GroupCount    int
+	InGarbage     bool
+	NextCancelled bool
+}
+
+func MakeParser() StreamParser {
+	return StreamParser{0, false, false}
+}
 
 type Puzzle struct{}
 
 func (p Puzzle) Test() string {
+	log.Println("----", ScoreGroups("{{{},{},{{}}}}"))
+	log.Println("----", ScoreGroups("{{<ab>},{<ab>},{<ab>},{<ab>}}"))
+	log.Println("----", ScoreGroups("{{<a!>},{<a!>},{<a!>},{<ab>}}"))
+	log.Println("----", ScoreGroups("{{<!!>},{<!!>},{<!!>},{<!!>}}"))
+	log.Println("----", ScoreGroups("<{o\"i!a,<{i<a>"))
+	log.Println("----", ScoreGroups("<<<<>"))
 	return "doin a test"
 }
 
 func (p Puzzle) Phase1() string {
-	return "doin phase1"
+	return strconv.Itoa(ScoreGroups(input))
 }
 
 func (p Puzzle) Phase2() string {
@@ -20,6 +41,35 @@ func (p Puzzle) Phase2() string {
 
 func (p Puzzle) String() string {
 	return "puzz" + id
+}
+
+func ScoreGroups(stream string) int {
+	p := MakeParser()
+	score := 0
+	for i, char := range stream {
+		if p.InGarbage {
+			switch {
+			case p.NextCancelled:
+				p.NextCancelled = false
+			case char == '!':
+				p.NextCancelled = true
+			case char == '>':
+				p.InGarbage = false
+			}
+		} else {
+			switch char {
+			case '{':
+				p.GroupCount++
+				score += p.GroupCount
+				log.Println(i, p.GroupCount)
+			case '}':
+				p.GroupCount--
+			case '<':
+				p.InGarbage = true
+			}
+		}
+	}
+	return score
 }
 
 func init() {
